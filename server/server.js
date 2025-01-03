3354d9dee314c89679e17f44439a6f7648787f4e
Baglan-maker <bagl203456@gmail.com>
2025-01-03 07:50:36 +0500

    configs

--- server/server.js
+++ server/server.js
@@ -38,5 +38,5 @@ app.use((err, req, res) => {
     res.status(500).send({ error: "Internal Server Error" });
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
