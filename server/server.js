7e873fff9743aae2489869099873bf4983ead7c6
Baglan-maker <bagl203456@gmail.com>
2025-01-03 13:42:59 +0500

    configs

--- server/server.js
+++ server/server.js
@@ -24,6 +24,7 @@ const corsOptions = {
             : [process.env.DEV_CLIENT_URL],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
 };
 
 app.use(cors(corsOptions));
